9a27429aa8006aa03d1c72495ec78ab09cdad01c
dustn078 <80445394+dustn078@users.noreply.github.com>
2023-02-27 00:46:13 +0900

    Add files via upload

--- inmoon_game.lua
+++ inmoon_game.lua
@@ -15,6 +15,22 @@ function scene:create( event )
 	inmoonGm.x, inmoonGm.y = display.contentWidth*0.5, display.contentHeight*0.5
 	sceneGroup:insert(inmoonGm)
 
+	---------점수-------------
+	local score = display.newText(10, display.contentWidth*0.1, display.contentHeight*0.15)
+	score.size = 100
+	score:setFillColor(0)
+	score.alpha = 0.5
+
+	sceneGroup:insert(score)
+
+	---------------F----------
+	local F = {}
+
+	for i=1, 5 do
+		F[i] = display.newImage("image/F.png")
+		F[i].x, F[i].y = display.contentWidth*0.5, display.contentHeight*0.12
+		F[i]:scale(0.5, 0.5)
+	end
 
 	-----------타이머--------------
 	local time = display.newText(10, display.contentWidth*0.9, display.contentHeight*0.15)
@@ -24,48 +40,47 @@ function scene:create( event )
 
 	sceneGroup:insert(time)
 
-	local function counter(event )
+	local function counter(event)
 		time.text = time.text - 1
 
-		if(time.text == 0) then
+		if(time.text == 1) then
 				composer.gotoScene("inmoon_ending")
-			end
 		end
 
-	---------점수-------------
-	local score = display.newText(10, display.contentWidth*0.1, display.contentHeight*0.15)
-	score.size = 100
-	score:setFillColor(0)
+		----------F 날리기--------------
+   		for i=1, 5 do
+   				local num = math.random(10)
+   				local w,h = display.contentWidth*num*0.1, display.contentHeight
+   				transition.moveTo( F[i], { x=w, y=display.contentHeight*0.8, time=3000*i})
 
-	sceneGroup:insert(score)
-
-	local function scroeCal(event)
-		score.text = score.text - 1
-
+   				if (cat.x == w) then
+   					score.text = score.text - 1
+   				end
+   		end
 	end
 
 	-----------설명서---------------
 	local message = display.newImage("image/피하기설명서.png")
 	message.x, message.y = display.contentWidth*0.5, display.contentHeight*0.5
 
-	local function tapEventListener(evnet)
+	local function tapEventListener(event)
 		message:removeSelf()
 		local timeAttack = timer.performWithDelay(1000, counter, 10) --설명서 누르면 시간 시작
 	end
 
 	message:addEventListener("tap", tapEventListener) -- 설명서 누르면 사라짐
 
+
+	-----------캐릭터 이미지-----------
 	local professor = display.newImage("image/교수캐릭터_화난모습.png")
 	professor.x, professor.y = display.contentWidth*0.5, display.contentHeight*0.12
 	professor:scale(0.5, 0.5)
 
 	local cat = display.newImage("image/고양이뒷모습.png")
-	cat.x, cat.y = display.contentWidth*0.5, display.contentHeight*0.88
+	cat.x, cat.y = display.contentWidth*0.5, display.contentHeight*0.8
 	cat:scale(0.5, 0.5)
 
 	message:toFront() -- 설명서를 제일 앞으로 
-
-
 
 	----------- 움직이는 모션 -----------
 	physics.start()
@@ -73,12 +88,12 @@ function scene:create( event )
 
 	local arrow = {}
 
-	arrow[1] = display.newImageRect("image/left.png", 38, 64)
-	arrow[1].x, arrow[1].y = display.contentWidth*0.9, display.contentHeight*0.8
+	arrow[1] = display.newImageRect("image/left.png", 70, 100)
+	arrow[1].x, arrow[1].y = display.contentWidth*0.87, display.contentHeight*0.8
 	arrow[1].name = "left"
 
-	arrow[2] = display.newImageRect("image/right.png", 38, 64)
-	arrow[2].x, arrow[2].y = arrow[1].x + 86,display.contentHeight*0.8
+	arrow[2] = display.newImageRect("image/right.png", 70, 100)
+	arrow[2].x, arrow[2].y = arrow[1].x + 90,display.contentHeight*0.8
 	arrow[2].name = "right"
 
 	arrow[3] = "right"
@@ -111,7 +126,6 @@ function scene:create( event )
 	end
 end
 
-
 	--------게임 종료 후------------
 
 function scene:show( event )
@@ -126,6 +140,7 @@ function scene:show( event )
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
 	end	
+	physics.start()
 end
 
 function scene:hide( event )
@@ -140,6 +155,7 @@ function scene:hide( event )
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
 	end
+	physics.pause()
 end
 
 function scene:destroy( event )
@@ -149,6 +165,7 @@ function scene:destroy( event )
 	-- 
 	-- INSERT code here to cleanup the scene
 	-- e.g. remove display objects, remove touch listeners, save state, etc.
+	 physics.stop()
 end
 
 ---------------------------------------------------------------------------------
