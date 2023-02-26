636cae6aa04ead8180b14acdac30bdeb7e61f30e
Kkimdoyeon <109220788+Kkimdoyeon@users.noreply.github.com>
2023-02-26 16:39:28 +0900

    Add files via upload

---
+++ mirogame/game.lua
@@ -0,0 +1,72 @@
+-----------------------------------------------------------------------------------------
+--
+-- view2.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+function scene:create( event )
+	local sceneGroup = self.view
+	
+	local background = display.newImageRect("image/minigame_background.jpg", display.contentWidth, display.contentHeight)
+ 	background.x, background.y = display.contentWidth/2, display.contentHeight/2
+
+	local ab_miro = display.newImage("image/about_miro.png")
+ 	ab_miro.x, ab_miro.y = display.contentWidth*0.5, display.contentHeight*0.5
+	
+	local button1 = display.newImage("image/button1.png")
+ 	button1.x, button1.y = display.contentWidth*0.9, display.contentHeight*0.9
+
+
+end
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if phase == "will" then
+		-- Called when the scene is still off screen and is about to move on screen
+	elseif phase == "did" then
+		-- Called when the scene is now on screen
+		-- 
+		-- INSERT code here to make the scene come alive
+		-- e.g. start timers, begin animation, play audio, etc.
+	end	
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if event.phase == "will" then
+		-- Called when the scene is on screen and is about to move off screen
+		--
+		-- INSERT code here to pause the scene
+		-- e.g. stop timers, stop animation, unload sounds, etc.)
+	elseif phase == "did" then
+		-- Called when the scene is now off screen
+	end
+end
+
+function scene:destroy( event )
+	local sceneGroup = self.view
+	
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	-- 
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
